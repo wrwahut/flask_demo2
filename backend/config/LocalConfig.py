f59bf9a795adf94e6c02b1d3987c2ac614e10172
root <root@localhost.localdomain>
2018-12-04 12:48:44 +0800

    first commit

---
+++ backend/config/LocalConfig.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+class local(object):
+     SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://root:qwer1234@localhost/wrwahut2?charset=utf8"
+     SQLALCHEMY_BINDS = {
+         "other": "mysql+mysqldb://cjb_out:cjb_out@47.105.55.39/cjbsp2?charset=utf8"
+     }
